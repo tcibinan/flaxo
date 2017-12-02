d54b9dde5599128f2b70b386193fcce711308393
Andrei Tcibin <andrei_tcibin@epam.com>
2017-12-02 14:44:52 +0300

    feat: Add basic data service implementation

---
+++ core/src/main/kotlin/com/tcibinan/flaxo/core/DataServiceImpl.kt
@@ -0,0 +1,56 @@
+package com.tcibinan.flaxo.core
+
+import com.tcibinan.flaxo.core.dao.CourseRepository
+import com.tcibinan.flaxo.core.dao.StudentRepository
+import com.tcibinan.flaxo.core.dao.TaskRepository
+import com.tcibinan.flaxo.core.dao.UserRepository
+import com.tcibinan.flaxo.core.model.Course
+import com.tcibinan.flaxo.core.model.CourseEntity
+import com.tcibinan.flaxo.core.model.CredentialsEntity
+import com.tcibinan.flaxo.core.model.Student
+import com.tcibinan.flaxo.core.model.StudentEntity
+import com.tcibinan.flaxo.core.model.TaskEntity
+import com.tcibinan.flaxo.core.model.User
+import com.tcibinan.flaxo.core.model.UserEntity
+import org.springframework.beans.factory.annotation.Autowired
+
+internal class DataServiceImpl : DataService {
+    @Autowired lateinit var userRepository: UserRepository
+    @Autowired lateinit var courseRepository: CourseRepository
+    @Autowired lateinit var taskRepository: TaskRepository
+    @Autowired lateinit var studentRepository: StudentRepository
+
+    override fun addUser(nickname: String,
+                         password: String): User =
+            userRepository.save(UserEntity(nickname, CredentialsEntity(password))).toDto()
+
+    override fun getUser(nickname: String): User? =
+            userRepository.findByNickname(nickname)
+
+    override fun createCourse(
+            name: String,
+            language: String,
+            testLanguage: String,
+            testingFramework: String,
+            numberOfTasks: Int,
+            owner: User): Course {
+        // TODO: rewrite using one to many approach. Now it might fall due to cascade type
+        val courseEntity = courseRepository.save(CourseEntity(name, language, testLanguage, testingFramework, owner.toEntity()))
+        for (i in 1..numberOfTasks) {
+            taskRepository.save(TaskEntity(name, courseEntity))
+        }
+        return getCourse(name, owner) ?: throw RuntimeException("Could not create the course. Check cascade types")
+    }
+
+    override fun getCourse(name: String,
+                           owner: User): Course? =
+            courseRepository.findByNameAndUser(name, owner)
+
+
+    override fun addStudent(nickname: String,
+                            course: Course): Student =
+            studentRepository.save(StudentEntity(nickname, course.toEntity())).toDto()
+
+    override fun getStudent(nickname: String): Student? =
+            studentRepository.findByNickname(nickname)
+}
