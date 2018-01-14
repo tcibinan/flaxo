f7359bc0661bd5479a85938f4f9fe18f00df4f32
Andrei Tcibin <andrei_tcibin@epam.com>
2018-01-15 00:02:21 +0300

    test: Create dependency type test

--- core/src/test/kotlin/com/tcibinan/flaxo/core/env/GradleBuildToolTest.kt
+++ core/src/test/kotlin/com/tcibinan/flaxo/core/env/GradleBuildToolTest.kt
@@ -1,9 +1,11 @@
 package com.tcibinan.flaxo.core.env
 
 import com.tcibinan.flaxo.core.env.tools.BuildTool
-import com.tcibinan.flaxo.core.env.tools.GradleDependency
-import com.tcibinan.flaxo.core.env.tools.GradlePlugin
-import com.tcibinan.flaxo.core.env.tools.GradleBuildTool
+import com.tcibinan.flaxo.core.env.tools.gradle.GradleDependency
+import com.tcibinan.flaxo.core.env.tools.gradle.GradlePlugin
+import com.tcibinan.flaxo.core.env.tools.gradle.GradleBuildTool
+import com.tcibinan.flaxo.core.env.tools.gradle.GradleDependencyType.COMPILE_TEST
+import com.tcibinan.flaxo.core.env.tools.gradle.GradleDependencyType.COMPILE_KOTLIN
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.matchers.shouldHave
 import io.kotlintest.matchers.substring
@@ -12,26 +14,30 @@ import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.jetbrains.spek.subject.SubjectSpek
 
-object GradleBuildToolSpec: SubjectSpek<BuildTool>({
+object GradleBuildToolSpec : SubjectSpek<BuildTool>({
     subject { GradleBuildTool() }
 
     val firstPlugin = GradlePlugin("java")
     val secondPlugin = GradlePlugin("application")
     val firstDependency = GradleDependency("a", "b", "c")
     val secondDependency = GradleDependency("1", "2", "3")
+    val testingDependency = GradleDependency("t", "y", "u", type = COMPILE_TEST)
+    val compileKotlinDependency = GradleDependency("t", "y", "u", type = COMPILE_KOTLIN)
 
     describe("Gradle build tool") {
         on("plugins addition") {
-            val buildTool = subject.addPlugin(firstPlugin)
-                    .addPlugin(secondPlugin)
-                    .addPlugin(secondPlugin)
+            val buildTool =
+                    subject.addPlugin(firstPlugin)
+                            .addPlugin(secondPlugin)
+                            .addPlugin(secondPlugin)
             val environment = buildTool.buildEnvironment()
-            val buildGradle = environment.getFiles().find { it.named("build.gradle") }
+            val buildGradle = environment.getFiles()
+                    .find { it.named("build.gradle") }
                     ?: throw Exception("build.gradle wasn't found")
 
             it("should have build.gradle with all passed plugins") {
                 buildGradle.content() shouldHave substring("plugins {")
-                buildGradle.shouldHave(firstPlugin, secondPlugin)
+                buildGradle.shouldHaveName(firstPlugin, secondPlugin)
             }
 
             it("should have build.gradle with a single line for repeated plugins") {
@@ -40,31 +46,48 @@ object GradleBuildToolSpec: SubjectSpek<BuildTool>({
         }
 
         on("dependency addition") {
-            val buildTool = subject.addDependency(firstDependency)
-                    .addDependency(secondDependency)
-                    .addDependency(secondDependency)
+            val buildTool =
+                    subject.addDependency(firstDependency)
+                            .addDependency(secondDependency)
+                            .addDependency(secondDependency)
             val environment = buildTool.buildEnvironment()
-            val buildGradle = environment.getFiles().find { it.named("build.gradle") }
+            val buildGradle = environment.getFiles()
+                    .find { it.named("build.gradle") }
                     ?: throw Exception("build.gradle wasn't found")
 
             it("should have build.gradle with build.gradle containing single dependency") {
-                buildGradle.shouldHave(firstDependency, secondDependency)
+                buildGradle.shouldHaveName(firstDependency, secondDependency)
             }
 
             it("should have build.gradle with a single line for repeated dependencies") {
-                buildGradle.shouldHaveSingle(secondDependency)
+                buildGradle.shouldHaveSingleName(secondDependency)
             }
+        }
+
+        on("different types of dependencies addition") {
+            val buildTool =
+                    subject.addDependency(testingDependency)
+                            .addDependency(compileKotlinDependency)
+            val environment = buildTool.buildEnvironment()
+            val buildGradle = environment.getFiles()
+                    .find { it.named("build.gradle") }
+                    ?: throw Exception("build.gradle wasn't found")
+
+            it("should contain all dependencies") {
+                buildGradle.shouldHaveDependency(testingDependency, compileKotlinDependency)
+            }
+
         }
     }
 })
 
 private fun File.named(name: String): Boolean = this.name() == name
 
-private fun File.shouldHave(vararg plugins: GradlePlugin) {
+private fun File.shouldHaveName(vararg plugins: GradlePlugin) {
     plugins.forEach {
         content() shouldHave
                 substring("apply plugin: \"${it.id}\"")
-                .or(substring("""id "${it.id}""""))
+                        .or(substring("""id "${it.id}""""))
     }
 }
 
@@ -78,17 +101,22 @@ private fun File.shouldHaveSingle(vararg plugins: GradlePlugin) {
     }
 }
 
-private fun File.shouldHave(vararg dependencies: GradleDependency) {
+private fun File.shouldHaveName(vararg dependencies: GradleDependency) {
     dependencies.forEach {
-        content() shouldHave
-                substring("${it.group}:${it.name}:${it.version}")
+        content() shouldHave substring(it.name())
     }
 }
 
-private fun File.shouldHaveSingle(vararg dependencies: GradleDependency) {
+private fun File.shouldHaveDependency(vararg dependencies: GradleDependency) {
+    dependencies.forEach {
+        content() shouldHave substring(it.toString())
+    }
+}
+
+private fun File.shouldHaveSingleName(vararg dependencies: GradleDependency) {
     dependencies.forEach {
         content()
-                .split("${it.group}:${it.name}:${it.version}")
+                .split(it.name())
                 .size - 1 shouldBe 1
     }
 }
