56c773fecbfe706a6877c39c60f685f99d9256f7
Andrei Tcibin <andrei_tcibin@epam.com>
2018-02-09 20:48:01 +0300

    fix: Add missing build tools interfaces

---
+++ core/src/main/kotlin/com/tcibinan/flaxo/core/build/BuildTool.kt
@@ -0,0 +1,23 @@
+package com.tcibinan.flaxo.core.build
+
+import com.tcibinan.flaxo.core.Environment
+import com.tcibinan.flaxo.core.NamedEntity
+import com.tcibinan.flaxo.core.framework.TestingFramework
+import com.tcibinan.flaxo.core.language.Language
+
+interface BuildTool : NamedEntity {
+
+    fun withLanguage(language: Language): BuildTool
+    fun withTestingsLanguage(language: Language): BuildTool
+    fun withTestingFramework(framework: TestingFramework): BuildTool
+
+    fun addDependency(dependency: Dependency): BuildTool =
+            throw UnsupportedOperationException(
+                    "${this::class} aka ${name()} doesn't have support for dependencies")
+
+    fun addPlugin(plugin: BuildToolPlugin): BuildTool =
+            throw UnsupportedOperationException(
+                    "${this::class} aka ${name()} doesn't have support for plugins")
+
+    fun buildEnvironment(): Environment
+}
---
+++ core/src/main/kotlin/com/tcibinan/flaxo/core/build/Dependency.kt
@@ -0,0 +1,5 @@
+package com.tcibinan.flaxo.core.build
+
+import com.tcibinan.flaxo.core.NamedEntity
+
+interface Dependency : NamedEntity
