48e7ef42b8998db5818aeb3241ffbdd192d4ef3c
Andrei Tcibin <andrei_tcibin@epam.com>
2018-02-10 12:42:30 +0300

    feat: Update gui github auth redirect handling

--- gui/src/main/resources/static/scripts.js
+++ gui/src/main/resources/static/scripts.js
@@ -1,30 +1,54 @@
 function authWithGithub() {
-    axios.get('rest/github/auth', {}, authHeaders())
-        .then(function (response) {
-            console.log(response);
-            console.log(response.data);
+    axios
+        .get('github/auth', {
+            baseURL: restUrl(),
+            auth: credentials()
         })
-        .catch(function (reason) {
-            console.log(reason);
-            console.log(reason.data);
-        });
+        .then(response => {
+            const payload = response.data.payload;
+            const params = new URLSearchParams();
+            const redirectParams = Immutable.Map(payload.params)
+                .forEach((value, key) => params.append(key, value));
+
+            window.location = payload.redirect + "?" + params.toString()
+        })
+        .catch(logResponse);
 }
 
-function authHeaders() {
+
+function echo() {
+    axios
+        .get('echo', {
+            baseURL: restUrl(),
+            auth: credentials(),
+            params: {
+                message: "helloworldsfsdfsdm"
+            }
+        })
+        .then(logResponse)
+        .catch(logResponse);
+}
+
+function logResponse(response) {
+    console.log(response);
+}
+
+function credentials() {
     return {
-        auth: {
-            username: username(),
-            password: password()
-        }
+        username: username(),
+        password: password()
     };
 }
 
 function username() {
-    return "uuuu";
-    // return document.querySelector("#nickname").value;
+    return document.querySelector("#nickname").value;
 }
 
 function password() {
-    return username();
-    // return document.querySelector("#password").value;
+    return document.querySelector("#password").value;
+}
+
+function restUrl() {
+    // TODO 10.02.18: Remove hardcoded url (set it with spring template manager f.e. thymeleaf)
+    return 'http://173.0.157.203:8080/rest';
 }
