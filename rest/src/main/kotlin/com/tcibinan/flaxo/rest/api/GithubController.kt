a892b8a7c8c8d09b2e7f7f8d37fef5eed018752a
Andrei Tcibin <andrei_tcibin@epam.com>
2018-02-10 21:20:29 +0300

    feat: Add github auth states temporary map for determining principals

--- rest/src/main/kotlin/com/tcibinan/flaxo/rest/api/GithubController.kt
+++ rest/src/main/kotlin/com/tcibinan/flaxo/rest/api/GithubController.kt
@@ -4,15 +4,19 @@ import com.tcibinan.flaxo.model.DataService
 import com.tcibinan.flaxo.model.IntegratedService
 import com.tcibinan.flaxo.rest.api.ServerAnswer.*
 import com.tcibinan.flaxo.rest.service.response.ResponseService
+import org.apache.commons.collections4.map.PassiveExpiringMap
 import org.apache.http.client.fluent.Form
 import org.apache.http.client.fluent.Request
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
+import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/rest/github")
@@ -22,35 +26,43 @@ class GithubController(
 ) {
 
     val githubAuthUrl = "https://github.com/login/oauth"
+    val states: MutableMap<String, String> = PassiveExpiringMap(
+            TimeUnit.MILLISECONDS.convert(5, TimeUnit.MINUTES)
+    )
     @Value("\${GITHUB_ID}")
     lateinit var clientId: String
     @Value("\${GITHUB_SECRET}")
     lateinit var clientSecret: String
 
     @GetMapping("/auth")
-    fun githubAuth(): Any {
+    @PreAuthorize("hasAuthority('USER')")
+    fun githubAuth(principal: Principal): Any {
+        val state = Random().nextInt().toString()
+
+        states[principal.name] = state
+
         return responseService.response(MANUAL_REDIRECT, payload = object {
             val redirect = "$githubAuthUrl/authorize"
             val params = mapOf(
                     "client_id" to clientId,
-                    "scope" to listOf("delete_repo", "repo").joinToString(separator = " ")
+                    "scope" to listOf("delete_repo", "repo").joinToString(separator = " "),
+                    "state" to state
             )
         })
     }
 
     @GetMapping("/auth/code")
     fun githubAuthToken(@RequestParam("code") code: String,
-                        @RequestParam("state") state: String,
-                        principal: Principal
+                        @RequestParam("state") state: String
     ) {
         Executors.newSingleThreadExecutor().execute {
-            Thread.sleep(2000)
             val accessToken = Request.Post("$githubAuthUrl/access_token")
                     .bodyForm(
                             Form.form().apply {
                                 add("client_id", clientId)
                                 add("client_secret", clientSecret)
                                 add("code", code)
+                                add("state", state)
                             }.build()
                     )
                     .execute()
@@ -62,7 +74,19 @@ class GithubController(
                     ?.last()
                     ?: throw Exception("Access token was not received from github.")
 
-            dataService.addToken(principal.name, IntegratedService.GITHUB, accessToken)
+            val nickname = synchronized(states) {
+                val key = states.filterValues { it == state }
+                        .apply {
+                            if (size > 1)
+                                throw Exception("Two users have the same random state for github auth.")
+                        }
+                        .keys.first()
+
+                states.remove(key)
+                key
+            }
+
+            dataService.addToken(nickname, IntegratedService.GITHUB, accessToken)
         }
     }
 }
