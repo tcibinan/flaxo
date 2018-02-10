dfd9d7c0649109d487e353062508a9cf5f91c27d
Andrei Tcibin <andrei_tcibin@epam.com>
2018-02-10 20:37:04 +0300

    feat: Make controller add access token to user credentials

--- rest/src/main/kotlin/com/tcibinan/flaxo/rest/api/GithubController.kt
+++ rest/src/main/kotlin/com/tcibinan/flaxo/rest/api/GithubController.kt
@@ -1,8 +1,9 @@
 package com.tcibinan.flaxo.rest.api
 
+import com.tcibinan.flaxo.model.DataService
+import com.tcibinan.flaxo.model.IntegratedService
 import com.tcibinan.flaxo.rest.api.ServerAnswer.*
 import com.tcibinan.flaxo.rest.service.response.ResponseService
-import org.apache.http.client.fluent.Content
 import org.apache.http.client.fluent.Form
 import org.apache.http.client.fluent.Request
 import org.springframework.beans.factory.annotation.Value
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 import java.util.concurrent.Executors
 
 @RestController
 @RequestMapping("/rest/github")
 class GithubController(
-        val responseService: ResponseService
+        private val responseService: ResponseService,
+        private val dataService: DataService
 ) {
 
     val githubAuthUrl = "https://github.com/login/oauth"
@@ -36,10 +39,13 @@ class GithubController(
     }
 
     @GetMapping("/auth/code")
-    fun githubAuthToken(@RequestParam("code") code: String, @RequestParam("state") state: String) {
+    fun githubAuthToken(@RequestParam("code") code: String,
+                        @RequestParam("state") state: String,
+                        principal: Principal
+    ) {
         Executors.newSingleThreadExecutor().execute {
             Thread.sleep(2000)
-            val content: Content = Request.Post("$githubAuthUrl/access_token")
+            val accessToken = Request.Post("$githubAuthUrl/access_token")
                     .bodyForm(
                             Form.form().apply {
                                 add("client_id", clientId)
@@ -49,8 +55,14 @@ class GithubController(
                     )
                     .execute()
                     .returnContent()
+                    .asString()
+                    .split("&")
+                    .find { it.startsWith("access_token") }
+                    ?.split("=")
+                    ?.last()
+                    ?: throw Exception("Access token was not received from github.")
 
-            println(content)
+            dataService.addToken(principal.name, IntegratedService.GITHUB, accessToken)
         }
     }
 }
