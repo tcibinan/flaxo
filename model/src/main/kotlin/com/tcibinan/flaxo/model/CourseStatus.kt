87354e3cc59aed66539578febc65341164a41c7d
Andrei Tcibin <andrei_tcibin@epam.com>
2018-03-07 01:17:10 +0300

    docs: Add KDoc for data services

--- model/src/main/kotlin/com/tcibinan/flaxo/model/CourseStatus.kt
+++ model/src/main/kotlin/com/tcibinan/flaxo/model/CourseStatus.kt
@@ -1,5 +1,15 @@
 package com.tcibinan.flaxo.model
 
+/**
+ * Interface represents course status.
+ *
+ * It could be [INIT]. In that case course hasn't started yet.
+ * The owner of the course is filling it with tasks and tests.
+ *
+ * It could be [RUNNING]. In that case course has already started.
+ * Students could send pull requests, all services and web hooks
+ * have been set up.
+ */
 interface CourseStatus {
     companion object {
         val INIT = "init"
--- model/src/main/kotlin/com/tcibinan/flaxo/model/IntegratedService.kt
+++ model/src/main/kotlin/com/tcibinan/flaxo/model/IntegratedService.kt
@@ -1,5 +1,8 @@
 package com.tcibinan.flaxo.model
 
+/**
+ * Services flaxo could have user access token for.
+ */
 enum class IntegratedService {
     GITHUB,
     TRAVIS
