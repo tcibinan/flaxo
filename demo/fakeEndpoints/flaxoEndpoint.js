51c40f0a1ab89e20f09a46a0c0d7d09471a12093
Andrei Tcibin <andrei_tcibin@epam.com>
2018-04-10 01:05:03 +0300

    feat: Add more data to demo http responses

--- demo/fakeEndpoints/flaxoEndpoint.js
+++ demo/fakeEndpoints/flaxoEndpoint.js
@@ -15,9 +15,9 @@ module.exports = SetupEndpoint({
                         id: 3312312,
                         githubId: "admin",
                         nickname: "admin",
-                        isGithubAuthorized: true,
-                        isTravisAuthorized: true,
-                        isCodacyAuthorized: true
+                        githubAuthorized: true,
+                        travisAuthorized: true,
+                        codacyAuthorized: true
                     }
                 }
             }]
@@ -69,8 +69,8 @@ module.exports = SetupEndpoint({
                             status: "running",
                             user: "admin",
                             userGithubId: "userGithubId",
-                            students: ["student1", "student2", "student3", "student4", "student5"],
-                            tasks: ["task1", "task2", "task3"]
+                            students: ["student1", "student2", "student3"],
+                            tasks: ["task1", "task2", "task3", "task4"]
                         }
                     ]
                 }
@@ -89,21 +89,25 @@ module.exports = SetupEndpoint({
                                     task: "task1",
                                     student: "student1",
                                     built: true,
-                                    succeed: true
+                                    succeed: true,
+                                    grade: "A",
+                                    deadline: true
                                 },
                                 {
                                     id: 12,
                                     task: "task2",
                                     student: "student1",
                                     built: true,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 },
                                 {
                                     id: 13,
                                     task: "task3",
                                     student: "student1",
                                     built: false,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 }
                             ],
                             student2: [
@@ -112,21 +116,25 @@ module.exports = SetupEndpoint({
                                     task: "task1",
                                     student: "student2",
                                     built: true,
-                                    succeed: true
+                                    succeed: true,
+                                    grade: "B",
+                                    deadline: true
                                 },
                                 {
                                     id: 22,
                                     task: "task2",
                                     student: "student2",
                                     built: true,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 },
                                 {
                                     id: 23,
                                     task: "task3",
                                     student: "student2",
                                     built: false,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 }
                             ],
                             student3: [
@@ -135,21 +143,25 @@ module.exports = SetupEndpoint({
                                     task: "task1",
                                     student: "student3",
                                     built: true,
-                                    succeed: true
+                                    succeed: false,
+                                    grade: "E",
+                                    deadline: true
                                 },
                                 {
                                     id: 32,
                                     task: "task2",
                                     student: "student3",
                                     built: true,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 },
                                 {
                                     id: 33,
                                     task: "task3",
                                     student: "student3",
                                     built: false,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 }
                             ],
                             student4: [
@@ -158,21 +170,25 @@ module.exports = SetupEndpoint({
                                     task: "task1",
                                     student: "student4",
                                     built: true,
-                                    succeed: true
+                                    succeed: true,
+                                    grade: "B",
+                                    deadline: false
                                 },
                                 {
                                     id: 42,
                                     task: "task2",
                                     student: "student4",
                                     built: true,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 },
                                 {
                                     id: 43,
                                     task: "task3",
                                     student: "student4",
                                     built: false,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 }
                             ],
                             student5: [
@@ -181,21 +197,25 @@ module.exports = SetupEndpoint({
                                     task: "task1",
                                     student: "student5",
                                     built: true,
-                                    succeed: true
+                                    succeed: true,
+                                    grade: "C",
+                                    deadline: true
                                 },
                                 {
                                     id: 52,
                                     task: "task2",
                                     student: "student5",
                                     built: true,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 },
                                 {
                                     id: 53,
                                     task: "task3",
                                     student: "student5",
                                     built: false,
-                                    succeed: false
+                                    succeed: false,
+                                    deadline: true
                                 }
                             ],
                         },
@@ -206,22 +226,39 @@ module.exports = SetupEndpoint({
                                     {
                                         students: {
                                             first: "student1",
-                                            second: "student2"
+                                            second: "student4"
                                         },
                                         lines: 50,
-                                        link: "http://plagiarismLink",
+                                        link: "http://plagiarismLink1",
                                         percentage: 85
                                     },
                                     {
                                         students: {
-                                            first: "student3",
+                                            first: "student2",
+                                            second: "student4"
+                                        },
+                                        lines: 15,
+                                        link: "http://plagiarismLink2",
+                                        percentage: 15
+                                    },
+                                    {
+                                        students: {
+                                            first: "student1",
                                             second: "student5"
                                         },
                                         lines: 21,
-                                        link: "http://plagiarismLink",
-                                        percentage: 43
+                                        link: "http://plagiarismLink3",
+                                        percentage: 22
                                     }
                                 ]
+                            },
+                            task2: {
+                                mossResultUrl: null,
+                                mossPlagiarismMatches: []
+                            },
+                            task3: {
+                                mossResultUrl: null,
+                                mossPlagiarismMatches: []
                             }
                         }
                     }
