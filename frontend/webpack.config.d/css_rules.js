786c3c031218c029bfb1d56e28b18c0461ce154c
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-01-27 22:39:14 +0300

    chore: Add automated flaxo version updating for frontend

--- frontend/webpack.config.d/css_rules.js
+++ frontend/webpack.config.d/css_rules.js
@@ -8,13 +8,16 @@ module.exports["module"]["rules"].push({
     loader: 'url-loader?limit=100000'
 });
 
+var execSync = require('child_process').execSync;
+var projectVersion = execSync("../../gradlew -q -p ../.. version").toString();
+
 module.exports["module"]["rules"].push({
     test: /\.js$/,
     loader: 'string-replace-loader',
     options: {
         multiple: [
             { search: '{{REST_URL}}', replace: process.env.REST_URL },
-            { search: '{{FLAXO_VERSION}}', replace: process.env.FLAXO_VERSION }
+            { search: '{{FLAXO_VERSION}}', replace: projectVersion }
         ]
     }
 });
