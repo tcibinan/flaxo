73e662768d05c81daea0da1512940576489c63b3
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-02-01 19:42:35 +0300

    feat: Create docker images publish scripts

---
+++ docker/images/frontend/publish.sh
@@ -0,0 +1,10 @@
+set -e
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Flaxo image tag should be specified as an argument'
+    exit 1
+fi
+
+tag=$1
+
+docker push tcibinan/flaxo-frontend:$tag
---
+++ docker/images/publish.sh
@@ -0,0 +1,12 @@
+set -e
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Flaxo image tag should be specified as an argument'
+    exit 1
+fi
+
+tag=$1
+
+./backend/publish.sh $tag
+
+./frontend/publish.sh $tag
