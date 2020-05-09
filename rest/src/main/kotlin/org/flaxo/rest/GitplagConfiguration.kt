270001fc742bb10af82656cc911f67a8f472a202
Andrei Tcibin <tsibin.andr@gmail.com>
2020-05-09 18:22:35 +0300

    Use conditional property for only gitplag analyser
    Currently all gitplag related classes depends on the chosen plagiarism analyser type. The commit fixes it and makes only gitplag plagiarism analyser depend on the property.

--- rest/src/main/kotlin/org/flaxo/rest/GitplagConfiguration.kt
+++ rest/src/main/kotlin/org/flaxo/rest/GitplagConfiguration.kt
@@ -16,13 +16,13 @@ import java.util.concurrent.TimeUnit
  * Gitplag configuration.
  */
 @Configuration
-@ConditionalOnProperty(name = ["flaxo.plagiarism.analyser"], havingValue = "gitplag")
 class GitplagConfiguration {
 
     /**
      * [GitplagPlagiarismAnalyser] bean
      */
     @Bean
+    @ConditionalOnProperty(name = ["flaxo.plagiarism.analyser"], havingValue = "gitplag")
     fun gitplagPlagiarismAnalyser(
             gitplagClient: GitplagClient,
             @Value("\${flaxo.gitplag.ui.url}") gitplagUiUrl: String
